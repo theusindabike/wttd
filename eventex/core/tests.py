2ca9c8c753fe28185428c47ed02f10e4812f41e2
Matheus Lopes <mlopes.matheus@gmail.com>
2019-11-26 15:07:51 -0300

    add subscriptions

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -3,13 +3,13 @@ from django.test import TestCase
 
 class HomeTest(TestCase):
     def setUp(self):
-        self.r = self.client.get("/")
+        self.resp = self.client.get("/")
 
     def test_get(self):
         """"GET / Must return status code 200 """
-        self.assertEqual(200, self.r.status_code)
+        self.assertEqual(200, self.resp.status_code)
 
 
     def test_template(self):
         """"Must use index.html"""
-        self.assertTemplateUsed(self.r, "index.html")
+        self.assertTemplateUsed(self.resp, "index.html")
