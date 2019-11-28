224c5461126c918341781e73fdbdb43e0820738e
Matheus Lopes <mlopes.matheus@gmail.com>
2019-11-28 16:25:18 -0300

    fix email settings

--- eventex/settings.py
+++ eventex/settings.py
@@ -125,7 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 
 #Email configuration
 EMAIL_BACKEND = config('EMAIL_BACKEND')
-EMAIL_HOST = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
