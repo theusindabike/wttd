42b288a53ed101a9f7be8e290c52fa3521f4e491
Matheus Lopes <mlopes.matheus@gmail.com>
2019-12-04 10:33:27 -0300

    add admin painel for subscription

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -20,3 +20,6 @@ class SubscriptionModelTest(TestCase):
 
     def test_created_at(self):
         self.assertIsInstance(self.obj.created_at, datetime)
+
+    def test_str(self):
+        self.assertEqual('Matheus Lopes', str(self.obj))
