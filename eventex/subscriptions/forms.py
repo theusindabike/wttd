34e3b39278eb5f16ca9e19c4fc6ef27cdb0ae52a
Matheus Lopes <mlopes.matheus@gmail.com>
2019-12-05 01:14:10 -0300

    add cpf validation

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,8 +1,17 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
+
+def validate_cpf(value):
+    if not value.isdigit():
+        raise ValidationError('Cpf deve conter apenas números', 'digits_error')
+
+    if len(value) != 11:
+        raise ValidationError('Cpf deve ter 11 números', 'length_error')
 
 
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label='Nome')
-    cpf = forms.CharField(label='CPF')
+    cpf = forms.CharField(label='CPF', validators=[validate_cpf])
     email = forms.EmailField(label='Email')
     phone = forms.CharField(label='Telefone')
--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -3,10 +3,34 @@ from eventex.subscriptions.forms import SubscriptionForm
 
 
 class SubscriptionFormTest(TestCase):
-    def setUp(self):
-        self.form = SubscriptionForm()
 
     def test_form_has_fields(self):
         """ Form must have 4 fields """
+        form = SubscriptionForm()
         expected = ['name', 'cpf', 'email', 'phone']
-        self.assertSequenceEqual(expected, list(self.form.fields))
+        self.assertSequenceEqual(expected, list(form.fields))
+
+    def test_cpf_is_digits(self):
+        form = self.make_validated_form(cpf='ABCD5678901')
+        self.assertFormErrorCode(form, 'cpf', 'digits_error')
+
+    def test_cpf_has_11_digits(self):
+        form = self.make_validated_form(cpf='1234')
+        self.assertFormErrorCode(form, 'cpf', 'length_error')
+
+    def assertFormErrorCode(self, form, field, code):
+        error_data = form.errors.as_data()
+        e = error_data[field][0]
+        self.assertEqual(code, e.code)
+
+    def make_validated_form(self, **kwargs):
+        valid = dict(
+            name='Matheus Lopes',
+            cpf='12345678901',
+            email='email@matheus.com',
+            phone='19999999999'
+        )
+        data = dict(valid, **kwargs)
+        form = SubscriptionForm(data)
+        form.is_valid()
+        return form
